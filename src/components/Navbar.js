b5ff3e82a763106b119d464efb72430ff37b7d8f
benjaminchong99 <pschong261199@hotmail.com>
2022-10-01 17:56:46 +0800

    added some parts for completion and workspace

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,16 @@ export default function Navbar() {
       <header className="bg-sky-900 md:sticky top-0 z-10">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-white">
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="/Home" className="mr-5 hover:text-black">
+          <a class="navbar" href="/Home" className="mr-5 hover:text-black">
             Home
           </a>
-          <a href="/Prereq" className="mr-5 hover:text-black">
+          <a class="navbar" href="/Prereq" className="mr-5 hover:text-black">
             PreReq
           </a>
-          <a href="/atWorkplace" className="mr-5 hover:text-black">
+          <a class="navbar" href="/atWorkplace" className="mr-5 hover:text-black">
             @Workplace
           </a>
-          <a href="/Completion" className="mr-5 hover:text-black">
+          <a class="navbar" href="/Completion" className="mr-5 hover:text-black">
             Completion
           </a>
           </nav>
